858defcb2928deb5e38bc38c68b1c0db57e9a4c2
anirudhpokala <anirudh@platform9.com>
2022-08-22 13:58:49 +0530

    Added backeded service first cut

---
+++ pkg/bugsnag/client.go
@@ -0,0 +1,38 @@
+package bugsnag
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// Type definition for struct encapsulating amplitude APIs.
+type api struct {
+	client  *http.Client
+	baseURL string
+}
+
+// getBugsnagInfoAPI fetches info using bork apis.
+func (api *api) getBugsnagInfoAPI() ([]byte, error) {
+	req, err := http.NewRequest("GET", api.baseURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Http request failed with error: %v", err)
+	}
+
+	//req.Header.Set("auth_token", token)
+	//req.Header.Set("Content-Type", "application/json")
+
+	resp, err := api.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed with error: %v", err)
+	}
+	// TODO: Should handle the status codes i.e if not OK
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read the data, error: %v", err)
+	}
+	return data, nil
+}
